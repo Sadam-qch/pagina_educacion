78d6354250d43cf1700fdb4c09ccfe7bf248e407
Maribel Choque <m.c.c.mabel21@gmail.com>
2025-03-10 12:18:08 -0400

    feat: se agrega proptitle al componente header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navegation from "../components/navegation";
 import Footer from "../components/Footer";
-import Header from "../components/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,7 +29,6 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Navegation />
-        <Header />
         {children}
         <Footer />
       </body>
