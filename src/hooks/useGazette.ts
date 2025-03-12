a60797851856f3df01dbc49021a04930e798284d
Maribel Choque <m.c.c.mabel21@gmail.com>
2025-03-12 12:57:37 -0400

    feat: se adiciono hooks para los enlaces

--- src/hooks/useGazette.ts
+++ src/hooks/useGazette.ts
@@ -4,6 +4,9 @@ import { useState, useEffect } from "react";
 
 export const useGazette = () => {
   const [gazette, setGazette] = useState<Gazette[]>();
+  const [modelo_biblio, setModBiblioteca] = useState<Gazette[]>();
+  const [modelo_kardex, setModKardex] = useState<Gazette[]>();
+  const [modelo_tus, setModTus] = useState<Gazette[]>();
   const [error, setError] = useState<string>();
   const [loading, setLoading] = useState<boolean>(false);
 
@@ -13,6 +16,27 @@ export const useGazette = () => {
         setLoading(true);
         const result = await institutionApi.getGazette();
         setGazette(result);
+        setModBiblioteca(
+          result.filter(
+            (data) =>
+              data.gaceta_tipo &&
+              data.gaceta_tipo === "MODELO DE CARTAS PARA BIBLIOTECA"
+          )
+        );
+        setModKardex(
+          result.filter(
+            (data) =>
+              data.gaceta_tipo &&
+              data.gaceta_tipo === "MODELO DE CARTAS PARA KARDEX"
+          )
+        );
+        setModTus(
+          result.filter(
+            (data) =>
+              data.gaceta_tipo &&
+              data.gaceta_tipo === "MODELOS DE CARTAS PARA EL TUS"
+          )
+        );
       } catch (error) {
         setError("Error al cargar las gacetas");
         console.error("Error fetching:", error);
@@ -24,5 +48,5 @@ export const useGazette = () => {
     fetchInstitution();
   }, []);
 
-  return { gazette, error, loading };
+  return { gazette, modelo_biblio, modelo_kardex, modelo_tus, error, loading };
 };
--- src/types/gazette.ts
+++ src/types/gazette.ts
@@ -3,5 +3,6 @@ export interface Gazette {
   gaceta_titulo: string;
   gaceta_fecha: string;
   gaceta_estado: number;
+  gaceta_tipo: string;
   gaceta_documento: string;
 }
