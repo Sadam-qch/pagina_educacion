e2d4be2402b2443560860df2e3848ae3ae89e8d5
Maribel Choque <m.c.c.mabel21@gmail.com>
2025-03-14 09:15:33 -0400

    feat(tramites): Se agrego contenido a la pagina con datos dinamicos

--- src/app/tramites/page.tsx
+++ src/app/tramites/page.tsx
@@ -3,16 +3,18 @@ import { FaFilePdf } from "react-icons/fa";
 import { useGazette } from "@/hooks/useGazette";
 export default function Page() {
   const { modelo_biblio } = useGazette();
+  const { modelo_kardex } = useGazette();
+  const { modelo_tus } = useGazette();
   return (
     <div className="flex flex-col items-center min-h-screen p-8 pt-24 bg-gray-200">
       {/* Título */}
       <h1 className="w-full py-4 text-3xl font-bold text-center bg-white rounded-lg shadow-md">
         TRAMITES 
       </h1>
-    {/* primera parte */}
       <h2 className="w-full py-4 text-3xl font-bold text-center ">
       Tramites en la Carrera de Educacion
       </h2>
+      {/* primera parte */}
       <div className="flex flex-col items-center mt-6 md:flex-row md:items-start md:space-x-8 ">
         {/* Imagen a la izquierda */}
         <div className="p-4 bg-white rounded-lg shadow-md md:w-1/2 hover:scale-105">
@@ -22,14 +24,13 @@ export default function Page() {
             className="object-cover w-full rounded-lg cursor-pointer "
           />
         </div>
-
         {/* Contenedor de botones */}
-        <div className="flex flex-col items-center mt-4 space-y-4 md:mt-0 md:w-1/3">
+        <div className="flex flex-col items-start mt-4 space-y-4 md:mt-0 md:w-1/2">
           <h2 className="text-xl font-semibold ">MODELO DE CARTAS PARA BIBLIOTECA</h2>
           
           {modelo_biblio && modelo_biblio.map((data) => (
             <a
-              key={data.gaceta_tipo}
+              key={data.gaceta_id}
               href={`${process.env.NEXT_PUBLIC_API_URL}/Gaceta/${data.gaceta_documento}`}
               target="_blank"
               rel="noopener noreferrer"
@@ -41,25 +42,8 @@ export default function Page() {
           ))}
         </div>
       </div>
-    {/* segunda parte */}
+      {/* segunda parte */}
       <div className="flex flex-col items-center mt-6 md:flex-row md:items-start md:space-x-8 ">
-        {/* Contenedor de botones */}
-        <div className="flex flex-col items-center mt-4 space-y-4 md:mt-0 md:w-1/3">
-          <h2 className="text-xl font-semibold ">MODELO DE CARTAS PARA KARDEX</h2>
-          
-          {[1, 2, 3].map((item) => (
-            <a
-              key={item}
-              href="https://serviciopagina.upea.bo/Gaceta/e249d024-9e08-4ac2-b91c-05ea4d88a757.pdf"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="flex items-center justify-center gap-4 px-5 py-1 text-lg font-semibold text-black bg-yellow-400 rounded-full shadow-lg hover:bg-yellow-500"
-            >
-              <FaFilePdf className="text-2xl text-red-600 " size={64} />
-              SOLICITUD DE CERTIFICADO
-            </a>
-          ))}
-        </div>
         {/* Imagen a la derecha */}
         <div className="p-4 bg-white rounded-lg shadow-md md:w-1/2 hover:scale-105">
           <img
@@ -67,6 +51,51 @@ export default function Page() {
             alt="Ejemplo"
             className="object-cover w-full rounded-lg cursor-pointer "
           />
+        </div>
+        {/* Contenedor de botones */}
+        <div className="flex flex-col items-start mt-4 space-y-4 md:mt-0 md:w-1/2">
+          <h2 className="text-xl font-semibold ">MODELO DE CARTAS PARA KARDEX</h2>
+          
+          {modelo_kardex && modelo_kardex.map((data) => (
+            <a
+              key={data.gaceta_id}
+              href={`${process.env.NEXT_PUBLIC_API_URL}/Gaceta/${data.gaceta_documento}`}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center justify-center gap-4 px-5 py-1 text-lg font-semibold text-black bg-yellow-400 rounded-full shadow-lg hover:bg-yellow-500"
+            >
+              <FaFilePdf className="text-2xl text-red-600 " size={64} />
+              {data.gaceta_titulo}
+            </a>
+          ))}
+        </div>
+      </div>
+      {/* tercera parte */}
+      <div className="flex flex-col items-center mt-6 md:flex-row md:items-start md:space-x-8 ">
+        {/* Imagen a la izquierda */}
+        <div className="p-4 bg-white rounded-lg shadow-md md:w-1/2 hover:scale-105">
+          <img
+            src="https://educacion.upea.edu.bo/img/TUS.c32021a8.jpg"
+            alt="Ejemplo"
+            className="object-cover w-full rounded-lg cursor-pointer "
+          />
+        </div>
+        {/* Contenedor de botones */}
+        <div className="flex flex-col items-start mt-4 space-y-4 md:mt-0 md:w-1/2">
+          <h2 className="text-xl font-semibold ">MODELO DE CARTAS PARA TUS</h2>
+          
+          {modelo_tus && modelo_tus.map((data) => (
+            <a
+              key={data.gaceta_id}
+              href={`${process.env.NEXT_PUBLIC_API_URL}/Gaceta/${data.gaceta_documento}`}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center justify-center gap-4 px-5 py-1 text-lg font-semibold text-black bg-yellow-400 rounded-full shadow-lg hover:bg-yellow-500"
+            >
+              <FaFilePdf className="text-2xl text-red-600 " size={64} />
+              {data.gaceta_titulo}
+            </a>
+          ))}
         </div>
       </div>
     </div>
