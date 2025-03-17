9b7f2a7701ee6aa8dcc7a6598d9d449c70eb1b8d
Maribel Choque <m.c.c.mabel21@gmail.com>
2025-03-17 13:10:19 -0400

    feat(useVideos): se agrego hooks para los enlaces

--- src/hooks/useVideos.ts
+++ src/hooks/useVideos.ts
@@ -5,6 +5,11 @@ import { useState, useEffect } from "react";
 export const useVideos = () => {
   const [videos, setVideos] = useState<Video[] | null>(null);
   const [noticia, setNoticia] = useState<Video[]>();
+  const [objetivoApoyoEducativo, setObjetivo] = useState<Video[]>();
+  const [apoyoPedagogico, setApoyoPedagogico] = useState<Video[]>();
+  const [orientacionVoc, setOrientacionVoc] = useState<Video[]>();
+  const [testimonio, setTestimonio] = useState<Video[]>();
+  const [actividades, setActividades] = useState<Video[]>();
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
 
@@ -21,6 +26,42 @@ export const useVideos = () => {
               data.video_tipo === "NOTICIA"
           )
         );
+        setObjetivo(
+          result.filter(
+            (data) =>
+              data.video_tipo &&
+              data.video_tipo === "OBJETIVO DEL APOYO EDUCATIVO"
+          )
+        );
+        setApoyoPedagogico(
+          result.filter(
+            (data) =>
+              data.video_tipo &&
+              data.video_tipo === "APOYO PEDAGOGICO"
+          )
+        );
+        setOrientacionVoc(
+          result.filter(
+            (data) =>
+              data.video_tipo &&
+              data.video_tipo === "ORIENTACION VOCACIONAL"
+          )
+        );
+        setTestimonio(
+          result.filter(
+            (data) =>
+              data.video_tipo &&
+              data.video_tipo === "TESTIMONIO"
+          )
+        );
+        setActividades(
+          result.filter(
+            (data) =>
+              data.video_tipo &&
+              data.video_tipo === "ACTIVIDADES"
+          )
+        );
+        
       } catch (error) {
         setError("Error al cargar los videos");
         console.error("Error fetching videos:", error);
@@ -30,5 +71,5 @@ export const useVideos = () => {
     fetchInstitution();
   }, []);
 
-  return { videos, noticia, loading, error };
+  return { videos, noticia, objetivoApoyoEducativo, apoyoPedagogico,orientacionVoc,testimonio,actividades, loading, error };
 };
