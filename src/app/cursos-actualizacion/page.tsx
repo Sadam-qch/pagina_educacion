ba4d4580fc2a0d3997f7a82f5249d367b2b21f13
Luis Choque <alfredochoque.c@gmail.com>
2025-03-28 12:30:57 -0400

    feat: se agrega pagina para cursos actualizacion

---
+++ src/app/cursos-actualizacion/page.tsx
@@ -0,0 +1,49 @@
+'use client'
+import Banner from "@/components/Banner";
+import { useVideos } from "@/hooks/useVideos";
+
+export default function Page() {
+    const { videos } = useVideos()
+    return (
+        <>
+            <Banner title="Cursos de Actualización" />
+            <main className="container-xxl py-5">
+                <div className="container">
+                    <div className="text-center wow fadeInUp" data-wow-delay="0.1s">
+                        <h6 className="section-title bg-white text-center text-primary px-3">Cursos de actualización</h6>
+                        <h1 className="mb-5">Lenguaje de Señas - Carrera de Ciencias de la Educación </h1>
+                    </div>
+                    {
+                        videos?.map((data, _i) => (
+                            <div className="container-xxl py-5">
+                                <div className="container">
+                                    <div className="row g-5">
+                                        <div className={`col-lg-6 wow fadeInUp ${_i % 2 === 0 ? 'order-1' : 'order-2'}`} data-wow-delay="0.1s">
+                                            <div className="ratio ratio-16x9">
+                                                <iframe src={data.video_enlace}
+                                                    title="YouTube video" ></iframe>
+                                            </div>
+                                        </div>
+                                        <div className={`col-lg-6 wow fadeInUp ${_i % 2 === 0 ? 'order-2' : 'order-1'}`} data-wow-delay="0.3s">
+                                            <h6 className="section-title bg-white text-start text-primary pe-3">
+                                                {data.video_tipo}
+                                            </h6>
+                                            <h1 className="mb-4">
+                                                {data.video_titulo}
+                                            </h1>
+                                            <div
+                                                dangerouslySetInnerHTML={{
+                                                    __html: data.video_breve_descripcion,
+                                                }}
+                                            />
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        ))
+                    }
+                </div>
+            </main>
+        </>
+    )
+} 
---
+++ src/mocks/videosAll/cursosActualizacion.ts
@@ -0,0 +1,20 @@
+
+
+const cursosActualizacion = [
+    {
+        "video_id": 39,
+        "video_enlace": "https://www.youtube.com/embed/osyXo50wlGw?si=-N-97jclGRmWBWqS",
+        "video_titulo": "Los Verbos",
+        "video_breve_descripcion": "<p>En Lengua de Señas, los verbos son signos que representan acciones o estados.</p><p>A diferencia de las lenguas orales, los verbos en lengua de señas pueden modificarse espacialmente para indicar sujetos, objetos y direcciones, lo que permite una gran expresividad y economía lingüística.</p>",
+        "video_tipo": "ACTIVIDADES",
+        "video_estado": 1
+    },
+    {
+        "video_id": 39,
+        "video_enlace": "https://www.youtube.com/embed/cKi8fiYApiE",
+        "video_titulo": "Conoce nuestras actividades",
+        "video_breve_descripcion": "<p>Actividades realizadas en la Gestión 2023 por el MSc. Deivid Pacosillo Mamani, Director de la Carrera de Ciencias de la Educación.</p><p>Entre los más resaltante se tiene la realización de Congresos Internacionales, Seminarios y Cursos de actualización, adquisición de equipos, actividades intercedes, ampliación de la Biblioteca.</p>",
+        "video_tipo": "ACTIVIDADES",
+        "video_estado": 1
+    },
+]
